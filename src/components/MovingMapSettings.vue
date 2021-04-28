6d587ed65cb0821eb50f7f8a3acb97bab6abf59b
elmatou <ma@brenac.org>
2021-04-28 15:42:33 +0200

    More MonvigMap settings avaliable

--- src/components/MovingMapSettings.vue
+++ src/components/MovingMapSettings.vue
@@ -35,6 +35,91 @@
     <b-dropdown-item @click="$emit('delete-track')">
       <b-icon icon="map-marker-remove-outline" />Delete trace
     </b-dropdown-item>
+    <b-dropdown-item separator />
+    <b-dropdown-item custom>
+      <b-field label="Trace length">
+        <b-numberinput
+          size="is-small"
+          controls-position="compact"
+          :editable="false"
+          step="50"
+          min="10"
+          v-bind:value="traceLength"
+          @input="$emit('update:settings', { traceLength: $event })"
+        >
+        </b-numberinput>
+      </b-field>
+    </b-dropdown-item>
+    <b-dropdown-item custom>
+      <b-field label="Futur postition delay (min.)">
+        <b-numberinput
+          size="is-small"
+          controls-position="compact"
+          :editable="false"
+          step="1"
+          min="0"
+          v-bind:value="futurPositionDelay"
+          @input="$emit('update:settings', { futurPositionDelay: $event })"
+        >
+        </b-numberinput>
+      </b-field>
+    </b-dropdown-item>
+    <b-dropdown-item custom>
+      <b-field label="Max. accuracy allowed">
+        <b-numberinput
+          size="is-small"
+          controls-position="compact"
+          :editable="false"
+          step="10"
+          min="1"
+          v-bind:value="maxAccuracy"
+          @input="$emit('update:settings', { maxAccuracy: $event })"
+        >
+        </b-numberinput>
+      </b-field>
+    </b-dropdown-item>
+    <b-dropdown-item custom>
+      <b-field label="Min. spd allowed">
+        <b-numberinput
+          size="is-small"
+          controls-position="compact"
+          :editable="false"
+          step=".05"
+          min="0"
+          v-bind:value="minSpeed"
+          @input="$emit('update:settings', { minSpeed: $event })"
+        >
+        </b-numberinput>
+      </b-field>
+    </b-dropdown-item>
+    <b-dropdown-item custom>
+      <b-field label="Min. dist. allowed">
+        <b-numberinput
+          size="is-small"
+          controls-position="compact"
+          :editable="false"
+          step="1"
+          min="0"
+          v-bind:value="minDistance"
+          @input="$emit('update:settings', { minDistance: $event })"
+        >
+        </b-numberinput>
+      </b-field>
+    </b-dropdown-item>
+    <b-dropdown-item custom>
+      <b-field label="Min. destination dist.">
+        <b-numberinput
+          size="is-small"
+          controls-position="compact"
+          :editable="false"
+          step="10"
+          min="1"
+          v-bind:value="minDestination"
+          @input="$emit('update:settings', { minDestination: $event })"
+        >
+        </b-numberinput>
+      </b-field>
+    </b-dropdown-item>
   </b-dropdown>
 </template>
 
@@ -46,6 +131,13 @@ export default {
     setView: Boolean,
     fullScreen: Boolean,
     zoomControl: Boolean,
+    traceLength: Number,
+    futurPositionDelay: Number,
+    maxAccuracy: Number,
+    minSpeed: Number,
+    minDistance: Number,
+    minDestination: Number,
+
     inFlight: Boolean
   }
 };
