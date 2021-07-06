8f91a6e5fa46d44b12fc24d18991020859f42c42
elmatou <ma@brenac.org>
2021-07-06 11:22:08 +0200

    Fix tablet breakpoint for navbar.

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,8 @@
 
 <style lang="scss">
 @import "bulmaswatch/flatly/_variables";
-// @import "bulma/sass/utilities/initial-variables";
-// $navbar-breakpoint: $tablet;
+@import "bulma/sass/utilities/initial-variables";
+$navbar-breakpoint: $tablet;
 $navbar-fixed-z: 2000;
 $modal-z: $navbar-fixed-z + 100;
 
